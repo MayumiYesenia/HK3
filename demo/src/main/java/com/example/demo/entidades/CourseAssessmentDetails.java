5aa64ad9f1cae1a8e137179b507c2f98440f75d3
MayumiYesenia <104656102+MayumiYesenia@users.noreply.github.com>
2023-10-29 09:05:16 -0500

    Update CourseAssessmentDetails.java
    clase

--- demo/src/main/java/com/example/demo/entidades/CourseAssessmentDetails.java
+++ demo/src/main/java/com/example/demo/entidades/CourseAssessmentDetails.java
@@ -1,5 +1,77 @@
 package com.example.demo.entidades;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class CourseAssessmentDetails {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Student student;
+
+    @ManyToOne
+    private Professor professor;
+
+    @ManyToOne
+    private CourseAssessment courseAssessment;
+
+    private String score;
+    private String section;
+
+    public CourseAssessmentDetails() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
+
+    public CourseAssessment getCourseAssessment() {
+        return courseAssessment;
+    }
+
+    public void setCourseAssessment(CourseAssessment courseAssessment) {
+        this.courseAssessment = courseAssessment;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
 }
