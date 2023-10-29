ae9f8e25b02a9f7be9b473c6b35a52b8c8bc756a
MayumiYesenia <104656102+MayumiYesenia@users.noreply.github.com>
2023-10-29 09:13:19 -0500

    Update CourseAssessmentDetailsController.java
    controllelr

--- demo/src/main/java/com/example/demo/controladores/CourseAssessmentDetailsController.java
+++ demo/src/main/java/com/example/demo/controladores/CourseAssessmentDetailsController.java
@@ -15,23 +15,53 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping("/course_assessment_details")
 public class CourseAssessmentDetailsController {
 
-    @GetMapping
+    @Autowired
+    private CourseAssessmentDetailsService courseAssessmentDetailsService;
 
+    @GetMapping
+    public ResponseEntity<List<CourseAssessmentDetails>> getAllCourseAssessmentDetails() {
+        List<CourseAssessmentDetails> courseAssessmentDetailsList = courseAssessmentDetailsService.findAll();
+        return ResponseEntity.ok(courseAssessmentDetailsList);
+    }
 
     @GetMapping("/{id}")
+    public ResponseEntity<CourseAssessmentDetails> getCourseAssessmentDetailsById(@PathVariable Long id) {
+        Optional<CourseAssessmentDetails> courseAssessmentDetails = courseAssessmentDetailsService.findById(id);
+        return courseAssessmentDetails.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
 
     @PostMapping
-
+    public ResponseEntity<CourseAssessmentDetails> createCourseAssessmentDetails(@RequestBody CourseAssessmentDetails courseAssessmentDetails) {
+        CourseAssessmentDetails createdDetails = courseAssessmentDetailsService.save(courseAssessmentDetails);
+        return ResponseEntity.status(201).body(createdDetails);
+    }
 
     @PutMapping("/{id}")
-
-
+    public ResponseEntity<CourseAssessmentDetails> updateCourseAssessmentDetails(@PathVariable Long id, @RequestBody CourseAssessmentDetails courseAssessmentDetails) {
+        if (!courseAssessmentDetailsService.findById(id).isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        courseAssessmentDetails.setId(id);
+        CourseAssessmentDetails updatedDetails = courseAssessmentDetailsService.save(courseAssessmentDetails);
+        return ResponseEntity.ok(updatedDetails);
+    }
 
     @DeleteMapping("/{id}")
-
+    public ResponseEntity<Void> deleteCourseAssessmentDetails(@PathVariable Long id) {
+        if (!courseAssessmentDetailsService.findById(id).isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        courseAssessmentDetailsService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
